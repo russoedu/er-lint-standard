00f8ee7f174e3aeb4891ed881af327fe1b57d9d5
Eduardo Russo <russoedu@gmail.com>
2026-02-15 23:52:41 +0000

    Eslint configured for JS/TS, JSON[C|5]?

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,24 @@
 import json from '@eslint/json'
+import pluginImport from 'eslint-plugin-import'
 import jsonc from 'eslint-plugin-jsonc'
+import pluginNode from 'eslint-plugin-n'
+import pluginPromise from 'eslint-plugin-promise'
 import { defineConfig } from 'eslint/config'
-import { jsonRules, jsonC5Rules } from './eslint.rules.json.mjs'
+import pluginTypescript from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import { importRules, nRules, promiseRules, standardOverrideRules, standardRules } from './eslint.rules.js.mjs'
+import { jsonC5Rules, jsonRules } from './eslint.rules.json.mjs'
+import { reactRules } from './eslint.rules.react.mjs'
+import { tsRules } from './eslint.rules.ts.mjs'
+import pluginReactRefresh from 'eslint-plugin-react-refresh'
+import globalsConfig from 'globals'
+
+const globals = {
+  ...globalsConfig.node,
+  ...globalsConfig.jest,
+  ...globalsConfig.browser,
+  // ...globals.jquery`
+}
 
 /* #region  CONFIG */
 export default defineConfig([
@@ -26,9 +43,55 @@ export default defineConfig([
     ],
   },
   /** -----------------------------------------
+  *  JS CONFIG
+  * ---------------------------------------- */
+  {
+    name:  'JS/TS rules',
+    files: [
+      '**/*.{js,mjs,cjs,jsx}',
+      '**/*.{ts,mts,cts,tsx}',
+    ],
+    extends: [
+      ...pluginTypescript.configs.stylisticTypeChecked, // OK here, now scoped only to TS
+    ],
+    settings: {
+      react: {
+        version: '19.2',
+      },
+    },
+    plugins: {
+      promise:              pluginPromise,
+      import:               pluginImport,
+      n:                    pluginNode,
+      '@typescript-eslint': pluginTypescript.plugin,
+      react:                pluginReact,
+      'react-refresh':      pluginReactRefresh,
+    },
+    languageOptions: {
+      globals,
+      parserOptions: {
+        project:             './tsconfig.json',
+        tsconfigRootDir:     import.meta.dirname,
+        allowDefaultProject: true,
+        ecmaFeatures:        { jsx: true },
+      },
+    },
+    rules: {
+      ...importRules,
+      ...nRules,
+      ...promiseRules,
+      ...standardRules,
+      // ...pluginTypescript.configs.recommended,
+      ...standardOverrideRules,
+      ...tsRules,
+      ...reactRules,
+    },
+  },
+  /** -----------------------------------------
   *  JSON CONFIG
   * ---------------------------------------- */
   {
+    name:  'JSON rules',
     files: [
       '**/*.json',
     ],
@@ -36,19 +99,33 @@ export default defineConfig([
       json,
       jsonc,
     },
-    rules: jsonRules,
+    language: 'json/json',
+    rules:    jsonRules,
   },
   {
+    name:  'JSONC rules',
     files: [
-      '**/*.json5',
       '**/*.jsonc',
-      '*.code-workspace'
     ],
     plugins: {
       json,
       jsonc,
     },
-    rules: jsonC5Rules,
+    language: 'json/jsonc',
+    rules:    jsonC5Rules,
+  },
+  {
+    name:  'JSON5 rules',
+    files: [
+      '**/*.json5',
+      '*.code-workspace',
+    ],
+    plugins: {
+      json,
+      jsonc,
+    },
+    language: 'json/json5',
+    rules:    jsonC5Rules,
   },
 ])
 /* #endregion */
---
+++ eslint.rules.react.mjs
@@ -0,0 +1,9 @@
+export const reactRules = {
+  'react-in-jsx-scope':                   'off',
+  'react/jsx-uses-react':                 'error',
+  'react/jsx-uses-vars':                  'error',
+  'react-refresh/only-export-components': [
+    'warn',
+    { allowConstantExport: true },
+  ],
+}
