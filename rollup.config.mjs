8179a531024b4ad6d5c103c9930fdf1de8172d64
Eduardo Russo <russoedu@gmail.com>
2026-02-19 00:05:57 +0000

    generating and updating config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,6 @@
 import typescript from '@rollup/plugin-typescript'
-// import terser from '@rollup/plugin-terser'
-// import eslint from '@rollup/plugin-eslint'
 import { readFileSync } from 'node:fs'
 import execute from 'rollup-plugin-shell'
-import { copy } from '@web/rollup-plugin-copy'
-import rename from '@betit/rollup-plugin-rename'
 
 const pack = JSON.parse(readFileSync('./package.json', 'utf8'))
 const dependencies = [
@@ -19,33 +15,13 @@ export default {
   output: {
     dir:            './',
     format:         'esm',
-    entryFileNames: 'eslint.linterall.new.mjs',
+    entryFileNames: 'eslint.linterall.mjs',
   },
   external,
   plugins: [
     typescript({
       tsconfig: 'tsconfig.eslint.json',
     }),
-    execute('eslint --fix eslint.linterall.new.mjs'),
-    rename({
-      include: ['eslint.linterall.new.mjs'],
-      map:     () => 'eslint.linterall.mjs',
-    }),
-
-    // eslint({
-    //   fix:                true,
-    //   overrideConfigFile: './eslint.config.mjs',
-    //   /* your options */
-    // }),
-    // terser({
-    //   format: {
-    //     comments: 'some',
-    //     beautify: true,
-    //     ecma:     '2024',
-    //   },
-    //   compress: false,
-    //   mangle:   false,
-    //   module:   true,
-    // }),
+    execute('eslint --fix eslint.linterall.mjs'),
   ],
 }
